a67ef9b9847670e50797c64a54a8cb83ef0d635c
abersheeran <me@abersheeran.com>
2022-03-20 00:43:33 +0800

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+FROM alpine:3.13.5
+
+ENV TZ "Asia/Shanghai"
+ENV V2RAY_VERSION v4.39.2
+ENV V2RAY_LOG_DIR /var/log/v2ray
+ENV V2RAY_CONFIG_DIR /etc/v2ray/
+ENV V2RAY_DOWNLOAD_URL https://github.com/v2fly/v2ray-core/releases/download/${V2RAY_VERSION}/v2ray-linux-64.zip
+
+WORKDIR /src
+
+RUN apk upgrade --update \
+    && apk add util-linux tzdata curl \
+    && mkdir -p ${V2RAY_LOG_DIR} ${V2RAY_CONFIG_DIR} /tmp/v2ray \
+    && curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip ${V2RAY_DOWNLOAD_URL} \
+    && pwd \
+    && unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray/ \
+    && mv /tmp/v2ray/v2ray /usr/bin \
+    && mv /tmp/v2ray/v2ctl /usr/bin \
+    && chmod +x /usr/bin/v2ray \
+    && chmod +x /usr/bin/v2ctl \
+    && ln -sf /usr/share/zoneinfo/${TZ} /etc/localtime \
+    && echo ${TZ} > /etc/timezone \
+    && rm -rf /tmp/v2ray /var/cache/apk/* \
+    && apk del curl
+
+COPY . /src
+
+ENTRYPOINT [ "sh", "/src/setup.sh"]
---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+domain="$1"
+
+if [ ! "$2" ] ;then
+    path="/confusion"
+else
+    path="$2"
+fi
+
+if  [ ! "$3" ] ;then
+    uuid=$(uuidgen)
+else
+    uuid="$3"
+fi
+
+sed -i -e "s@HOST@${domain}@" -e "s@UUID@${uuid}@" -e "s@PATH@${path}@" /src/v2ray.json
+
+echo "域名："$domain
+echo "UUID："$uuid
+echo "路径："$path
+echo ""
+echo "安卓客户端 v2rayNG 配置链接："
+echo -e "vmess://\c"
+echo '{"add":"DOMAIN","aid":"64","host":"","id":"UUID","net":"ws","path":"PATH","port":"443","ps":"DOMAIN","sni":"","tls":"tls","type":"auto","v":"2"}' | sed -e "s@DOMAIN@${domain}@" -e "s@DOMAIN@${domain}@" -e "s@PATH@${path}@" -e "s@UUID@${uuid}@" | base64
+echo "电脑客户端 v2rayN 配置链接："
+echo -e "vmess://\c"
+echo '{"v": "2", "ps": "", "add": "DOMAIN", "port": "443", "id": "UUID", "aid": "64", "scy": "none", "net": "ws", "type": "auto", "host": "", "path": "PATH", "tls": "tls", "sni": "DOMAIN"}' | sed -e "s@DOMAIN@${domain}@" -e "s@DOMAIN@${domain}@" -e "s@PATH@${path}@" -e "s@UUID@${uuid}@" | base64
+
+
+/usr/bin/v2ray -config /src/v2ray.json
